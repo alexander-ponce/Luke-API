4842c2dc8030d18bea28f528aced7f7e142eba99
alexander-ponce <alex@poncealex.com>
2023-05-03 13:36:44 -0700

    Updated

---
+++ client/src/components/Form.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const Form = () => {
+  return (
+    <div>Form</div>
+  )
+}
+
+export default Form
---
+++ client/src/components/People.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const People = () => {
+    
+        const [people, setPeople] = useState([]);
+     
+        useEffect(() => {
+            fetch('https://swapi.dev/api/people/')
+                .then(response => response.json())
+                .then(response => setPeople(response.results))
+        }, []);
+     
+        return (
+            <div>
+                {people.length > 0 && people.map((person, index)=>{
+                    return (<div key={index}>{person.name}</div>)
+                })}
+            </div>
+        );
+    }
+export default People
